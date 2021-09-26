d97cb021b19392e1607747948ad586d98a225ea6
Sadman2539 <cadetsadman39@gmail.com>
2021-09-27 01:10:18 +0800

    code optimized

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -10,31 +10,31 @@ const Course = (props) => {
 
 
     return (
-        <div>
-            <div className="card">
-                <div className="card-img">
-                    <img src={image} alt="" />
-                </div>
-                <div className="card-body">
-                    <h3>{courseName.slice(0, 45)}...</h3>
-                    <p>by {author}</p>
 
-                    ({star}) <Rating
-                        initialRating={star}
-                        emptySymbol="far fa-star rating-icon"
-                        fullSymbol="fas fa-star rating-icon"
-                        readonly
-                    />  <span>({enrolled})</span>
-                    <h3>${price}</h3>
-                    <button className="regular-btn" onClick={() => props.handleAddToButton(props.course)}>
-                        <FontAwesomeIcon className="btn-icon" icon={faShoppingCart} />
-                        Add to cart</button>
-                    <button className="regular-btn">
-                        <i className="fas fa-heart btn-icon " aria-hidden="true"></i>
-                        Wishlist</button>
-                </div>
+        <div className="card">
+            <div className="card-img">
+                <img src={image} alt="" />
+            </div>
+            <div className="card-body">
+                <h3>{courseName.slice(0, 45)}...</h3>
+                <p>by {author}</p>
+
+                ({star}) <Rating
+                    initialRating={star}
+                    emptySymbol="far fa-star rating-icon"
+                    fullSymbol="fas fa-star rating-icon"
+                    readonly
+                />  <span>({enrolled})</span>
+                <h3>${price}</h3>
+                <button className="regular-btn" onClick={() => props.handleAddToButton(props.course)}>
+                    <FontAwesomeIcon className="btn-icon" icon={faShoppingCart} />
+                    Add to cart</button>
+                <button className="regular-btn">
+                    <i className="fas fa-heart btn-icon " aria-hidden="true"></i>
+                    Wishlist</button>
             </div>
         </div>
+
     );
 };
 
